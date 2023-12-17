611fcccd8b862e39c6660d5075a4c92bd48584ea
AliHassanEventHorizon <contacts.alihassan@gmail.com>
2023-12-17 21:45:34 +0500

    first commit

---
+++ Chat.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const socketIo = require("socket.io");
+const port = process.env.PORT || 3000;
+const app = express();
+const cors = require('cors');
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*", 
+    },
+});
+
+app.use(cors()); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/index2", (req, res) => {
+    res.sendFile(__dirname + "/index2.html");
+});
+
+let messageforward = ""
+let seacondcomming = ""
+io.on("connection", (socket) => {
+    socket.on("transferControl", (data) => {
+        let receivedData = data.Messagedata;
+        messageforward = receivedData;
+
+        if (messageforward != "") {
+            console.log("First Signal", messageforward);
+            io.emit("Transmission", { firstsignal: messageforward });
+            messageforward = "";
+        }
+    });
+
+    socket.on("CalltotheHorizon", (data) => {
+        let HorzonSignal = data.Seacondmessagedata;
+        seacondcomming = HorzonSignal;
+
+        if (seacondcomming != "") {
+            console.log("Seacond Signal", seacondcomming);
+            io.emit("Horizonresponse", { seacondsignal: seacondcomming });
+            seacondcomming = "";
+        }
+    });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+  console.log("Server is listening");
+});
