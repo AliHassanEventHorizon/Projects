90af163305f07a7fb692ea02b4d13c20d880763a
AliHassanEventHorizon <154075910+AliHassanEventHorizon@users.noreply.github.com>
2023-12-18 14:36:23 +0500

    a

--- Chat.js
+++ Chat.js
@@ -6,35 +6,33 @@ const serverless = require('serverless-http');
 const app = express();
 app.use(cors());
 
-// Attach Socket.IO directly to the app
+
 const io = socketIo(app, {
   cors: {
     origin: "*",
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello, serverless!");
+app.get("/Server", (req, res) => {
+    io.on("connection", (socket) => {
+        console.log("A user connected");
+      
+        socket.on("transferControl", (data) => {
+          const receivedData = data.Messagedata;
+          console.log("First Signal", receivedData);
+      
+          io.emit("Transmission", { firstsignal: receivedData });
+        });
+      
+        socket.on("CalltotheHorizon", (data) => {
+          const HorizonSignal = data.Seacondmessagedata;
+          console.log("Seacond Signal", HorizonSignal);
+      
+          io.emit("Horizonresponse", { seacondsignal: HorizonSignal });
+        });
+      });
 });
 
-// Handle WebSocket connections
-io.on("connection", (socket) => {
-  console.log("A user connected");
 
-  socket.on("transferControl", (data) => {
-    const receivedData = data.Messagedata;
-    console.log("First Signal", receivedData);
 
-    io.emit("Transmission", { firstsignal: receivedData });
-  });
-
-  socket.on("CalltotheHorizon", (data) => {
-    const HorizonSignal = data.Seacondmessagedata;
-    console.log("Seacond Signal", HorizonSignal);
-
-    io.emit("Horizonresponse", { seacondsignal: HorizonSignal });
-  });
-});
-
-// Convert the app to a serverless function
 module.exports.handler = serverless(app);
